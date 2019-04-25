658ea4efd5e3d1a5040718913c0af5affd339704
Damian Drągowski <damian.dragowski@gmail.com>
2019-04-25 11:45:10 +0200

    list with index

--- src/test/kotlin/com/ovalinfo/starter/StarterApplicationTests.kt
+++ src/test/kotlin/com/ovalinfo/starter/StarterApplicationTests.kt
@@ -23,8 +23,12 @@ class StarterApplicationTests {
 	@Test
 	fun contextLoads() {
 		val m = mapOf("key1" to 1, "key2" to 2, "key3" to 3)
+		val l = listOf(2,2,3,5,5)
 		for ( (k,v) in m ) {
 			println("key = $k value = $v")
+		}
+		for ( (index,value) in l.withIndex() ) {
+			println("" + index + " value = " +value)
 		}
 		logger.debug(cachingConnectionFactoryCA.toString())
 		logger.debug("test")
